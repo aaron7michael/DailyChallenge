66ce2334256c162ea77206f4f56f9b503bff692d
Aaron Stadler <aaron.stadler@infor.com>
2026-01-06 10:47:01 -0500

    add problem explanation and method signatures

---
+++ DailyChallenge/Jan62026.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// From: https://www.geeksforgeeks.org/problems/max-xor-subarray-of-size-k/1
+/// Given an array of integers arr[]  and a number k. Return the maximum xor of a subarray of size k.
+/// Input: arr[] = [2, 5, 8, 1, 1, 3], k = 3
+/// Output: 15
+/// Explanation: arr[0] ^ arr[1] ^ arr[2] = 15, which is maximum.
+/// 
+/// Note: A subarray is a contiguous part of any given array.
+/// </summary>
+public class MaxXORFromArray
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public int GetMaxXORFromArray(int[] arr, int k);
+    public int[] GetRandomIntArray(int size)
+    {
+        
+    }
+}
+
